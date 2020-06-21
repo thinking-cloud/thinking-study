0ceeb3809db3a1ae0abb70200c0ab062113bec9b
thinking-cloud <thinking-cloud-2020@outlook.com>
2020-06-22 01:44:34 +0800

    服务端/客户端注册

---
+++ thinking-study-spring-cloud/eureka-server/src/main/java/eureka/server/started/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package eureka.server.started;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Server 默认启动类
+ * @author thinking-cloud
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
